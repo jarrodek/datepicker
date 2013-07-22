042de5cc46c91131d6aded398fe7f971ab98ffea
jarrodek <jarrodek@gmail.com>
2013-07-22 02:04:37 +0200

    Working example

--- lib/components/daterange.dart
+++ lib/components/daterange.dart
@@ -9,9 +9,30 @@ import 'package:intl/intl_browser.dart';
 import 'package:intl/intl.dart';
 
 class DateRange extends WebComponent{
+  ///Applied selection end
+  @observable DateTime selectionend;
+  ///Applied selection start
+  @observable DateTime selectionstart;
+  
+  
   @observable bool initialized = false;
+  @observable bool showing = false;
   ///Value to be displayed in display
-  String get displayValue => (_dateFormat == null || startDate == null || endDate == null) ? '' : '${_dateFormat.format(startDate)} - ${_dateFormat.format(endDate)}';
+  String get displayValue {
+    if(selectionstart == null){
+      //Must have selection start
+      return "";
+    }
+    String start = _dateFormat.format(selectionstart);
+    String end;
+    if(selectionend == null){
+      end = start;
+    } else {
+      end = _dateFormat.format(selectionend);
+    }@observable DateTime currentSelectedStart;
+    return "$start - $end";
+  }
+  
   /// Display date format
   @observable DateFormat _dateFormat = new DateFormat.yMd();
   /// Current locale
@@ -19,35 +40,59 @@ class DateRange extends WebComponent{
   String get locale => _locale;
   set locale(String l){
     _locale = l;
-    
     initializeDateFormatting(_locale, null)
-      .then((_) => _dateFormat = new DateFormat.yMd(_locale))
-      .then((_) => _initializeTexts(new DateFormat.E().dateSymbols));
+      .then((_) => _initializeTexts(new DateFormat.E(l).dateSymbols))
+      .then((_) => _dateFormat = new DateFormat.yMd(l));
   }
   
-  @observable int firstDayOfWeek;
-  @observable static List<String> monthTexts;
-  @observable static List<String> weekdayTexts;
+  int firstDayOfWeek;
+  @observable static List<String> monthTexts = toObservable([]);
+  @observable static List<String> weekdayTexts = toObservable([]);
   set format(String f) => new DateFormat(f,locale);
   
   /// Currently selected (but not accepted) start day
-  @observable DateTime selectedStartDate;
-  /// Currently selected (but not accepted) end day
-  @observable DateTime selectedEndDate;
-  
-  @observable DateTime startDate = new DateTime.now();
-  @observable DateTime endDate = new DateTime.now();
-   
+  @observable DateTime currentSelectedStart;
+  ///String value for selected date start
   String _startDateInput;
-  String _endDateInput;
-  String get startDateInput => _startDateInput == null ? _dateFormat.format(selectedStartDate) : _startDateInput;
-  String get endDateInput => _endDateInput == null ? selectedEndDate == null ? '' : _dateFormat.format(selectedEndDate) : _endDateInput;
+  /// Fetter for selected date start.
+  /// If [_startDateInput] is null, then [currentSelectedStart] will be taken to format date.
+  String get startDateInput => _startDateInput == null ? currentSelectedStart == null ? '' : _dateFormat.format(currentSelectedStart) : _startDateInput;
+  /// Currently selected (but not accepted) end day
+  @observable DateTime currentSelectedEnd;
+  @observable String _endDateInput;
+  @observable String get endDateInput => _endDateInput == null ? currentSelectedEnd == null ? '' : _dateFormat.format(currentSelectedEnd) : _endDateInput;
+  
+  /// Visible calendars start date
+  @observable DateTime calendarStartDate = new DateTime.now();
+  /// True if user has it's own custom range and false if the user used preseleced values.
+  @observable bool isCustomSelection = true;
+  String get inputRangeCss => isCustomSelection ? '' : ' disabled';
   
   set startDateInput(String v){
     try{
-      selectedStartDate = _dateFormat.parse(v);
-      _startDateInput = v; //_dateFormat.format(selectedStartDate);
-      startDate = selectedStartDate;
+      DateTime _try = _dateFormat.parse(v);
+      
+      if(limitright != null){
+        DateTime compare = new DateTime(limitright.year,limitright.month, limitright.day);
+        if(_try.isAfter(compare)){
+          currentSelectedStart = compare;
+          _startDateInput = _dateFormat.format(compare);
+          return;
+        }
+      }
+      if(limitleft != null){
+        DateTime compare = new DateTime(limitleft.year, limitleft.month, limitleft.day);
+        if(_try.isBefore(compare)){
+          currentSelectedStart = compare;
+          _startDateInput = _dateFormat.format(compare);
+          return;
+        }
+      }
+      
+      
+      currentSelectedStart = _try;
+      _startDateInput = v;
+      calendarStartDate = new DateTime(currentSelectedStart.year, currentSelectedStart.month+2,currentSelectedStart.day);
     }catch(e){
       print('Formated string: $v, locale: $locale');
       print(e);
@@ -55,9 +100,27 @@ class DateRange extends WebComponent{
   }
   set endDateInput(String v){
     try{
-      selectedEndDate = _dateFormat.parse(v);
+      DateTime _try = _dateFormat.parse(v);
+      if(limitright != null){
+        DateTime compare = new DateTime(limitright.year,limitright.month, limitright.day);
+        if(_try.isAfter(compare)){
+          currentSelectedEnd = compare;
+          _endDateInput = _dateFormat.format(compare);
+          return;
+        }
+      }
+      if(limitleft != null){
+        DateTime compare = new DateTime(limitleft.year, limitleft.month, limitleft.day);
+        if(_try.isBefore(compare)){
+          currentSelectedEnd = compare;
+          _endDateInput = _dateFormat.format(compare);
+          return;
+        }
+      }
+      currentSelectedEnd = _try;
       _endDateInput = v;
-      endDate = selectedEndDate;
+      //
+      calendarStartDate = currentSelectedEnd;
     }catch(e){
       print('Formated string: $v, locale: $locale');
       print(e);
@@ -72,28 +135,28 @@ class DateRange extends WebComponent{
   int calendars = 3;
   @observable List<AppCalendar> get  calendarsList => _calendarsData();
   
-  DateRange(){
-    DateTime d = new DateTime.now();
-    selectedStartDate = new DateTime(d.year, d.month, d.day);
-    selectedEndDate = new DateTime(d.year, d.month, d.day);
-  }
-    
   
   List<AppCalendar> _calendarsData(){
     if(!initialized) return [];
     
     List<AppCalendar> result = [];
     
-    if(startDate.month == endDate.month){
-      //startDate.month must be in the middle
-      startDate = new DateTime(startDate.year, startDate.month-1, 1).subtract(new Duration(days:1));
-    } else if(endDate.month-startDate.month == 2){
-    } else {
-      startDate = new DateTime(startDate.year, startDate.month+1, 1).subtract(new Duration(days:1));
+    if(calendars > 3){
+      calendars = 3;
     }
     
+    
+    int m = calendarStartDate.month;
+    int y = calendarStartDate.year;
+    
+    m -= (calendars-1);
+    if(m < 1){ //months are [1-12] based
+      m = 12 + m; //m is only 0 to -11
+      y--;
+    }
+    DateTime startDate = new DateTime(y, m, 1);
+    
     for(int i=0; i<calendars; i++){
-      
       int y = startDate.year;
       int m = startDate.month;
       m += i;
@@ -177,15 +240,32 @@ class DateRange extends WebComponent{
   }
   
   void created(){
+    currentSelectedStart = _getNow();
+    currentSelectedEnd = currentSelectedStart;
+    
     findSystemLocale()
       .then((_) => initializeDateFormatting(Intl.systemLocale, null))
       .then((_) => _dateFormat = new DateFormat.yMd(Intl.systemLocale))
       .then((_) => _initializeTexts(new DateFormat.E().dateSymbols));
   }
-  void _initializeTexts(DateSymbols ds){
+  
+  void inserted(){
+    if(selectionstart != null){
+      currentSelectedStart = new DateTime(selectionstart.year, selectionstart.month, selectionstart.day);
+      if(selectionend != null){
+        currentSelectedEnd = new DateTime(selectionend.year, selectionend.month, selectionend.day);;
+      } else {
+        currentSelectedEnd = selectionstart;
+      }
+    }
+  }
+  
+  Future _initializeTexts(DateSymbols ds){
+    //print("Initialize text layer: $ds");
     firstDayOfWeek = ds.FIRSTDAYOFWEEK;
-    weekdayTexts = [];
-    for(int i=0; i<7; i++){
+    //print("The first day of week is: $firstDayOfWeek");
+    weekdayTexts.clear();
+    for(int i=1; i<=7; i++){
       int k = firstDayOfWeek+i;
       if(k>=7)
         k = k - 7;
@@ -193,52 +273,149 @@ class DateRange extends WebComponent{
     }
     monthTexts = ds.STANDALONESHORTMONTHS;
     initialized = true;
+    return new Future.value();
   }
   
   void previousMonth(){
-    startDate = new DateTime(startDate.year, startDate.month-1, startDate.day);
+    calendarStartDate = new DateTime(calendarStartDate.year, calendarStartDate.month-1, calendarStartDate.day);
   }
   
   void nextMonth(){
-    startDate = new DateTime(startDate.year, startDate.month+1, startDate.day);
+    calendarStartDate = new DateTime(calendarStartDate.year, calendarStartDate.month+1, calendarStartDate.day);
   }
   
   bool firstDaySelected = false;
   void selectDay(int year, int month, int day){
     DateTime d = new DateTime(year, month, day);
+    setAsCustomSelection();
+    
     if(firstDaySelected){
-      selectedEndDate = d;
+      // If new selection is earlier than [currentSelectedStart] then replace the dates.
+      if(d.isBefore(currentSelectedStart)){
+        currentSelectedStart = d;
+        currentSelectedEnd = d;
+        return;
+      }
+      
+      currentSelectedEnd = d;
       firstDaySelected = false;
     } else {
-      selectedEndDate = null;
-      selectedStartDate = d;
+      // select both start and end range as the same date.
+      currentSelectedEnd = d;
+      currentSelectedStart = d;
       firstDaySelected = true;
     }
   }
   
   bool isSelected(int year, int month, int day){
     DateTime current = new DateTime(year, month, day);
-    
-    if(selectedStartDate == null){
+    if(currentSelectedStart == null){
       return false;
     }
     
-    if(current.isAtSameMomentAs(selectedStartDate)){
+    // Need to copy object or it can contain hours, minutes and seconds which cause vary results
+    DateTime compare = new DateTime(currentSelectedStart.year,currentSelectedStart.month, currentSelectedStart.day);
+    
+    
+    if(current.isAtSameMomentAs(compare)){
       return true;
     }
     
-    if(current.isAfter(selectedStartDate)){
+    if(current.isAfter(compare)){
       //check left bound
-      if(selectedEndDate == null){
+      if(currentSelectedEnd == null){
         return false;
       }
-      if(current.isBefore(selectedEndDate) || current.isAtSameMomentAs(selectedEndDate)){
+      DateTime compareEnd = new DateTime(currentSelectedEnd.year,currentSelectedEnd.month, currentSelectedEnd.day);
+      if(current.isBefore(compareEnd) || current.isAtSameMomentAs(compareEnd)){
         return true;
       }
     }
     return false;
-    
   }
+  /// Set current selection as a result
+  void apply(Event e){
+    selectionstart = currentSelectedStart;
+    selectionend = currentSelectedEnd;
+    showing = false;
+    
+    
+    
+    Event ce = new Event.eventType('HTMLEvents','change');
+    host.dispatchEvent(ce);
+  }
+  /// Cancel dialog
+  void cancel(Event e){
+    e.preventDefault();
+    showing = false;
+  }
+  
+  void show(Event e){
+    e.preventDefault();
+    showing = true;
+  }
+  
+  void rangeInputsChange(Event e){
+    e.preventDefault();
+    e.stopPropagation();
+  }
+  
+  void presetRange(Event e){
+    e.preventDefault();
+    e.stopPropagation();
+    SelectElement target = e.target;
+    List<OptionElement> items = target.selectedOptions;
+    if(items.length == 0) return;
+    OptionElement item = items.first;
+    switch(item.value){
+      case 'today': 
+        currentSelectedStart = _getNow();
+        currentSelectedEnd = currentSelectedStart;
+        isCustomSelection = false;
+        firstDaySelected = false;
+      break;
+      case 'yesterday': 
+        currentSelectedStart = _getNow().subtract(new Duration(days:1));
+        currentSelectedEnd = currentSelectedStart;
+        isCustomSelection = false;
+        firstDaySelected = false;
+        break;
+      case 'last-week':
+        DateTime helper = _getNow();
+        currentSelectedStart = helper.subtract(new Duration(days: (7 + helper.weekday))); //remove 7 days and day of the week number
+        currentSelectedEnd = helper.subtract(new Duration(days: (1 + helper.weekday))); // remove day of the week and one day
+        isCustomSelection = false;
+        firstDaySelected = false;
+        break;
+      case 'last-month': 
+        DateTime helper = _getNow();
+        currentSelectedStart = new DateTime(helper.year,helper.month-1,1);
+        currentSelectedEnd = new DateTime(helper.year,helper.month,1).subtract(new Duration(days:1));
+        isCustomSelection = false;
+        firstDaySelected = false;
+        break;
+      case 'custom': 
+        //nothing
+        isCustomSelection = true;
+        firstDaySelected = false;
+        break;
+    }
+  }
+  
+  void setAsCustomSelection(){
+    isCustomSelection = true;
+    Element el = query('#DrPresetOption');
+    if(el == null) return;
+    (el as SelectElement).selectedIndex = 0;
+  }
+  
+  
+  /// Get current day [DateTime] object without hours, minutes, seconds and so on. 
+  DateTime _getNow(){
+    DateTime helper = new DateTime.now();
+    return new DateTime(helper.year,helper.month,helper.day);
+  }
+  
 }
 
 
--- web/index.dart
+++ web/index.dart
@@ -1,7 +1,16 @@
+import 'dart:html';
+
 String date1 = "2013-07-01";
 String date2;
 String date3 = "1999-01-01";
 String date4;
+
 DateTime dateRangeRight = new DateTime.now();
+DateTime dateSelectionRight = new DateTime.now().subtract(new Duration(days: 2));
+DateTime dateSelectionLeft = new DateTime.now().subtract(new Duration(days: 12));
 main(){
+}
+
+void callbackAction(Event e){
+ print("Callback event action: ${e.type}"); 
 }
